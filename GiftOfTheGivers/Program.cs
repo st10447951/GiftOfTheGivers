536ff35693f144f9b0cc9f20f41a7886ba590d31
Senzo Nkonyane <162983632+st10447951@users.noreply.github.com>
2026-09-25 11:17:17 +0200

    secong commit

--- GiftOfTheGivers/Program.cs
+++ GiftOfTheGivers/Program.cs
@@ -32,7 +32,6 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
     await DbSeeder.SeedAsync(scope.ServiceProvider);
 }
 
